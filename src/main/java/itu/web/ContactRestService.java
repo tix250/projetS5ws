5ab3605e6c71666a607be6be994a8b37e58c0826
Andrianjafison Heritiana <83717072+tix200@users.noreply.github.com>
2022-01-15 18:39:31 +0300

    maj 2

--- src/main/java/itu/web/ContactRestService.java
+++ src/main/java/itu/web/ContactRestService.java
@@ -2,8 +2,12 @@ package itu.web;
 
 import java.awt.List;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +24,17 @@ public class ContactRestService {
 	public ArrayList<Contact> getContact()
 	{
 		return  (ArrayList<Contact>) contactRepository.findAll();
+	}
+	
+	@RequestMapping(value="/contact/{id}",method=RequestMethod.GET)
+	public Optional<Contact> getContact(@PathVariable Long id)
+	{
+		return contactRepository.findById(id);
+	}
+	
+	@RequestMapping(value="/contact",method=RequestMethod.POST)
+	public Contact save (@RequestBody Contact c)
+	{
+		 return contactRepository.save(c);
 	}
 }
